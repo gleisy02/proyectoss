025f4eeafcf13118308c5f51e951d8075365cd3d
gleisy02 <elizabethgleisy@gmail.com>
2026-07-28 17:17:33 -0400

    primer parcial

---
+++ primerparcial/src/App.tsx
@@ -0,0 +1,16 @@
+ import "./App.css";
+ import logo from "./assets/hero.png";
+
+ function App() {
+    
+    return (
+        <div className="App">
+            <h1> Gleisy Valerio </h1>
+            <img src={logo} alt="hero" />
+            <p> RUKUKUKU</p>
+            <button> Haz clic aqui </button>
+        </div>
+    );
+}
+
+export default App;
